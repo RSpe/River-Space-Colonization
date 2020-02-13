bce4ddc5fc6848438d047aababc20bc27b5840c3
unknown <reubenspeirs@gmail.com>
2020-02-13 19:00:46 +1300

    Adding files from learnt from tutorial

---
+++ River-Space-Colonization/VertexArray.cpp
@@ -0,0 +1,39 @@
+#include "VertexArray.h"
+#include "VertexBufferLayout.h"
+
+#include"Renderer.h"
+
+VertexArray::VertexArray()
+{
+	GLCall(glGenVertexArrays(1, &m_RendererID));
+}
+
+VertexArray::~VertexArray()
+{
+	GLCall(glDeleteVertexArrays(1, &m_RendererID));
+}
+
+void VertexArray::AddBuffer(const VertexBuffer& vb, const VertexBufferLayout& layout)
+{
+	Bind();
+	vb.Bind();
+	const auto& elements = layout.GetElements();
+	unsigned int offset = 0;
+	for (unsigned int i = 0; i < elements.size(); i++)
+	{
+		const auto& element = elements[i];
+		GLCall(glEnableVertexAttribArray(i));
+		GLCall(glVertexAttribPointer(i, element.type, element.count, element.normalised, layout.GetStride(), (const void*)offset)); // Index 0 of vao, bound to currently bound array buffer, set index to 1 and can bind to a different buffer
+		offset += element.count * VertexBufferElement::GetSizeOfType(element.type);
+	}
+}
+
+void VertexArray::Bind() const
+{
+	GLCall(glBindVertexArray(m_RendererID));
+}
+
+void VertexArray::Unbind() const
+{
+	GLCall(glBindVertexArray(0));
+}
